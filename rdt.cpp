b28d499912b3f28a1c5d2499e8b7ee73db94db02
ChadDavies <davies.chad@gmail.com>
2015-03-29 17:33:39 -0700

    Refactored Send, recieve

--- rdt.cpp
+++ rdt.cpp
@@ -34,56 +34,71 @@ int get_num_packets(int buffer_length)
   return num_packets;
 }
 
+packet* recv_buffer_to_packets(int num_packets, int socket_descriptor,char *buffer, int buffer_length, int flags, struct sockaddr *from_address, int *address_length)
+{
+  //Create array of packets
+  packet* packets = new packet[num_packets];
+
+  //Load data into packets
+  for(int i = 0; i < num_packets; i++)
+  {
+    char local_buffer[PACKET_SIZE];
+    int recieveLength = recvfrom(socket_descriptor, &local_buffer, PACKET_SIZE, flags, from_address, (socklen_t*)address_length);
+    printf("RDT Recieved Length %d\n",recieveLength);
+    if (recieveLength == -1)
+    {
+      printf("RDT Recieved Failed.\n");
+      return 0;
+    }
+
+
+    memcpy(&packets[i], local_buffer, PACKET_SIZE);
+
+     printf("RDT Recieved Packed Data %s\n\n\n", packets[i].data);
+
+  }
+
+  return packets;
+
+}
+
+void copy_packets_to_buffer(packet* packets, char* buffer, int num_packets, int buffer_length)
+{
+  //transfer the packet data into the buffer
+  for(int i = 0; i < num_packets; i++)
+  {
+    for(int d = 0; d < PACKET_DATA_SIZE; d++)
+    {
+      int data_index = i * PACKET_DATA_SIZE + d;
+      if(data_index < buffer_length)
+      {
+        buffer[data_index] = packets[i].data[d];
+      }
+    }
+  }
+  buffer[buffer_length] = '\0';
+    printf("RDT Recieved Message %s\n\n\n", buffer);
+}
+
 int rdt_recv(int socket_descriptor,char *buffer, int buffer_length, int flags, struct sockaddr *from_address, int *address_length)
 {
     //Get number of packets to recieve
     int num_packets = get_num_packets(buffer_length);
 
-    //Create array of packets
-    packet* packets = new packet[num_packets];
+    packet* packets = recv_buffer_to_packets(num_packets, socket_descriptor, buffer, buffer_length, flags, from_address, address_length);
 
-    //Load data into packets
-    for(int i = 0; i < num_packets; i++)
+    if (packets == 0)
     {
-      char local_buffer[PACKET_SIZE];
-      int recieveLength = recvfrom(socket_descriptor, &local_buffer, PACKET_SIZE, flags, from_address, (socklen_t*)address_length);
-      printf("RDT Recieved Length %d\n",recieveLength);
-      if (recieveLength == -1)
-      {
-        printf("RDT Reieved Failed.\n");
-        return recieveLength;
-      }
-
-
-      memcpy(&packets[i], local_buffer, PACKET_SIZE);
-
-      // printf("RDT Recieved Packed Data %s\n\n\n", packets[i].data);
-
+      return -1;
     }
 
-    //transfer the packet data into the buffer
-    for(int i = 0; i < num_packets; i++)
-    {
-      for(int d = 0; d < PACKET_DATA_SIZE; d++)
-      {
-        int data_index = i * PACKET_DATA_SIZE + d;
-        if(data_index < buffer_length)
-        {
-          buffer[data_index] = packets[i].data[d];
-        }
-      }
-    }
-    buffer[buffer_length] = '\0';
-    //  printf("RDT Recieved Message %s\n\n\n", buffer);
+    copy_packets_to_buffer(packets, buffer, num_packets, buffer_length);
 
   return buffer_length;
 }
 
-int rdt_sendto(int socket_descriptor,char *buffer,int buffer_length,int flags,struct sockaddr * destination_address,int address_length)
+packet* copy_buffer_to_packets(int num_packets, char *buffer, int buffer_length)
 {
-  //Get Number of packets to send
-  int num_packets = get_num_packets(buffer_length);
-
   //Create array of packets to send
   packet* packets = new packet[num_packets];
 
@@ -103,7 +118,11 @@ int rdt_sendto(int socket_descriptor,char *buffer,int buffer_length,int flags,st
       }
     }
   }
+  return packets;
+}
 
+void send_packets(int socket_descriptor,char *buffer,int buffer_length,int flags,struct sockaddr * destination_address,int address_length, int num_packets, packet* packets)
+{
   //Send packets
   for(int i = 0; i < num_packets; i++)
   {
@@ -114,6 +133,17 @@ int rdt_sendto(int socket_descriptor,char *buffer,int buffer_length,int flags,st
         printf("RDT Couldn't be sent.\n");
     }
   }
+}
+
+int rdt_sendto(int socket_descriptor,char *buffer,int buffer_length,int flags,struct sockaddr * destination_address,int address_length)
+{
+  //Get Number of packets to send
+  int num_packets = get_num_packets(buffer_length);
+
+  packet* packets = copy_buffer_to_packets(num_packets, buffer, buffer_length);
+
+  send_packets(socket_descriptor, buffer, buffer_length, flags, destination_address, address_length, num_packets, packets);
+
   return buffer_length;
 }
 
